f48f1e2c1368dc504a98b6a9b08907e69c1029b1
sanix-darker <s4nixd@gmail.com>
2023-12-12 06:21:45 +0100

    feat: no need for eval, call the execution directly

--- shell/key-bindings.bash
+++ shell/key-bindings.bash
@@ -106,7 +106,7 @@ __tmux_search__(){
 __path_cmd_search__(){
     source ~/.bash_aliases && \
         CMD_TO_BE_EXECUTED="$(tac $PWD/.cmd_history | fzf --height ${FZF_TMUX_HEIGHT:-40%})" && \
-        echo $CMD_TO_BE_EXECUTED && eval $CMD_TO_BE_EXECUTED
+        echo $CMD_TO_BE_EXECUTED && $CMD_TO_BE_EXECUTED
 }
 
 # Required to refresh the prompt after fzf
